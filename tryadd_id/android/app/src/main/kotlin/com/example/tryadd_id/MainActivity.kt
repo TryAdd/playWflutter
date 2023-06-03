c1df6c3b462d335198a1bdcc02329b00d81cfd98
Try Add <tryadd@Trys-MacBook-Pro.local>
2023-06-03 03:18:13 +0300

    Done

---
+++ tryadd_id/android/app/src/main/kotlin/com/example/tryadd_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tryadd_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tryadd_id/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: TryAdd(),
+    ));
+
+class TryAdd extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text(
+          "TryAdd ID",
+          style: TextStyle(
+            color: Colors.grey[400],
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 50.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/avatar.JPG'),
+                radius: 90,
+              ),
+            ),
+            const SizedBox(height: 50),
+            Text(
+              "Name",
+              style: TextStyle(
+                color: Colors.grey[200],
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              "Ahmed HISHAM",
+              style: TextStyle(
+                color: Colors.cyanAccent[100],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            Text(
+              "Position",
+              style: TextStyle(
+                color: Colors.grey[200],
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              "Application Developmer",
+              style: TextStyle(
+                color: Colors.teal[400],
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              ),
+            ),
+            const SizedBox(height: 50.0),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.teal[200],
+                  size: 29,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  "Tryadd@gmail.com",
+                  style: TextStyle(
+                    color: Colors.grey[200],
+                    letterSpacing: 1.5,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
